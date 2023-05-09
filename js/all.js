653741e9fe21baefebade7d70dec102ce8780875
cantarback <18163996546@163.com>
2023-05-09 16:01:45 +0800

    可访问项目

---
+++ js/all.js
@@ -0,0 +1,19 @@
+// var bottom_log = document.getElementById('bottom_log');
+var header_logo = document.getElementsByClassName('header-logo')[0];
+var main_top_r_btn = document.getElementsByClassName('main_top_r_btn')[0];
+var main_top_l_b_xgj = document.getElementById('main_top_l_b_xgj');
+// bottom_log.onclick = function () {
+//     window.location.href = 'index.html';
+// }
+
+header_logo.onclick = function () {
+    window.location.href = 'index.html';
+}
+
+main_top_r_btn.onclick = function () {
+    window.location.href = 'index.html';
+}
+
+main_top_l_b_xgj.onclick = function () {
+    window.location.href = 'index.html';
+}
